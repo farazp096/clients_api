c10053579db3280b489fc9fcfb80e5455ce99d1b
Faraz khan <farazp096@gmail.com>
2022-06-19 14:48:56 +0530

    add comments

--- controllers/client.controllers.js
+++ controllers/client.controllers.js
@@ -1,7 +1,7 @@
 const Client = require("../models/client.models");
 
 
-// ---------------------------- add new new client ---------------------------------
+// ---------------------------- add new client ---------------------------------
 
 exports.addClient = async (req,res,next) => {
   const {email,name,note,apartment,address,contact,pinCode,cardId,tokenId,last4digits} = req.body
@@ -18,22 +18,33 @@ exports.addClient = async (req,res,next) => {
         name:name,
         contact:contact,
         note:note,
-        addresses:[{
+
+        addresses:[             // client can have multiple addresses
+
+        // adress 1
+            {                   
             apartment:apartment,
             address:address,
             picCode:pinCode
         },
-        {
+
+        // hard coded address 2 just to show multiple addresses
+        {                                 
             apartment:"another apartment",
             address:"another address",
             picCode:5678
         }
     ],
-        cards:[{
+        cards:[                    // client can have multiple cards details as well 
+
+        // card 1
+            {
             cardId:cardId,
             tokenId:tokenId,
             last4Digits:last4digits
         },
+
+        // hard coded card 2 just to show multiple cards
         {
             cardId:9747963270743,
             tokenId:"another tokenId",
@@ -54,6 +65,8 @@ exports.addClient = async (req,res,next) => {
   
 };
 
+// ---------------------------- get all clients -----------------------------
+
 exports.showAllClients = (req,res)=>{
     Client.find((err,clients)=>{
         if(err){
@@ -64,6 +77,8 @@ exports.showAllClients = (req,res)=>{
         }
     })
 }
+
+// ------------------------------ get single client by id ------------------------
 
 exports.showClient = (req,res)=>{
     Client.findOne({_id:req.params.id},(err,client)=>{
--- routes/client.routes.js
+++ routes/client.routes.js
@@ -2,18 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    addClient, showAllClients , showClient, logOutUser
+    addClient, showAllClients , showClient
 
 } = require("../controllers/client.controllers");
 
 router.get("/addClient",(req,res)=>{
     res.render("register",{title:"Add Client"})
 })
-
+//----------------------------- add client ----------------------
 router.post("/addClient", addClient);
 
+
+//----------------------------- get all clients --------------------
 router.get("/getAllClients",showAllClients)
 
+
+//------------------------------ get single client by id --------------------
 router.get("/client/:id",showClient)
 
 
